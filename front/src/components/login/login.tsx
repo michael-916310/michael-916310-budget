df329773e9feb852bf90a9a86f1ba17427c9a8b3
michael-916310 <916310@rambler.ru>
2021-06-13 13:53:20 +0400

    fix: нормально добавил normalize

--- front/src/components/login/login.tsx
+++ front/src/components/login/login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card } from 'antd';
+import {Card } from 'antd';
 
 import styles from './login.module.scss';
 import {useAppDispatch} from './../../app/hooks';
@@ -8,10 +8,9 @@ import {useAppDispatch} from './../../app/hooks';
 export default function Login(){
   return (
     <main className={styles.main}>
-      <Card title="Введите код" style={{ width: 300 }}>
-        <p>Card content</p>
-        <p>Card content</p>
-      </Card>
+      <section>
+        ВВЕДИТЕ КОД
+      </section>
     </main>
   )
 }
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -4,6 +4,8 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
 
 import './index.scss';
+import 'normalize.css';
+
 import store from './app/store';
 import App from './App';
 
