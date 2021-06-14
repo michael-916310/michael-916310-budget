fadc583be87e9829785abcfda0a56c3edba0680d
michael-916310 <916310@rambler.ru>
2021-06-14 12:28:38 +0400

    feat: деплой для проверки на сотовом

--- front/src/components/login/login.tsx
+++ front/src/components/login/login.tsx
@@ -1,30 +1,44 @@
 import React, { useState } from 'react';
-import { Input, Button } from 'antd'
+import { Input, Button } from 'antd';
+import { ArrowLeftOutlined, SendOutlined } from '@ant-design/icons';
 
 import styles from './login.module.scss';
 import {useAppDispatch} from './../../app/hooks';
-import { setConstantValue } from 'typescript';
 
 type btnType = {
-  display: string;
-  displayIcon: string;
+  display?: string;
+  displayIcon?: any;
   value: string;
-  action: ()=>void;
+  action: ((item: btnType)=>void) | (()=>void);
 }
 
-const btnArray:btnType[]=[
-  {display:'0', displayIcon:'0', value:'0', action:()=>{}},
-  {display:'1', displayIcon:'0', value:'1', action:()=>{}},
-  {display:'2', displayIcon:'0', value:'2', action:()=>{}},
-  {display:'3', displayIcon:'0', value:'3', action:()=>{}},
-];
+
 
 export default function Login(){
   const [value, setValue] = useState('');
 
-  function handleBtnPress(el:btnType) {
-    setValue(`${value}${el.value}`);
+  function handleBtnPress(item:btnType) {
+    setValue(`${value}${item.value}`);
   }
+
+  function backspaceBtnPress() {
+    setValue(value.substring(0,value.length-1));
+  }
+
+  const btnArray:btnType[]=[
+    {display:'1', value:'1', action:handleBtnPress},
+    {display:'2', value:'2', action:handleBtnPress},
+    {display:'3', value:'3', action:handleBtnPress},
+    {display:'4', value:'4', action:handleBtnPress},
+    {display:'5', value:'5', action:handleBtnPress},
+    {display:'6', value:'6', action:handleBtnPress},
+    {display:'7', value:'7', action:handleBtnPress},
+    {display:'8', value:'8', action:handleBtnPress},
+    {display:'9', value:'9', action:handleBtnPress},
+    {display:'0', value:'0', action:handleBtnPress},
+    {displayIcon:<ArrowLeftOutlined/>, value:'backspace', action:backspaceBtnPress},
+    {displayIcon:<SendOutlined/>, value:'send', action:()=>{}},
+  ];
 
   return (
     <main className={styles.main}>
@@ -32,21 +46,22 @@ export default function Login(){
         <article className={styles.inputControlWrapper}>
           <Input
             readOnly={true}
-            size='large'
             className={styles.inputControl}
             value={value}/>
         </article>
         <article className={styles.btnContainer}>
           {
-            btnArray.map((el) => {
-              return <Button
+            btnArray.map((item) => (
+                <Button
+                key = {item.value}
                 shape='round'
-                onClick={()=>{handleBtnPress(el)}}
+                onClick = {()=>{item.action(item)}}
                 className={styles.btn}
               >
-                  {el.display}
-              </Button>;
-            })
+                {(item.display) ? (item.display) : (item.displayIcon)}
+              </Button>
+              )
+            )
           }
         </article>
       </section>
