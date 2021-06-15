b6dd10642106c7a1e2167ff99f0cebbb7be9e2a1
michael-916310 <916310@rambler.ru>
2021-06-15 11:14:01 +0400

    feat: back: добавил создание и загрузку справочников в базу

--- back/src/bin/www.js
+++ back/src/bin/www.js
@@ -1,14 +1,14 @@
 const app = require('../app');
 const { DEV_PORT } = require('../common/config');
 const { connectToDB } = require('../common/db/mongodb');
-const { mySQLIsAlive, mySQLPool } = require('./../common/db/mySqlDb');
+const logger = require('./../common/logging/logger');
+
+require('./../common/db/mySqlDb');
 
 const port = process.env.PORT || DEV_PORT;
 
 app.listen(port, () =>
-  console.log(`server listening at http://localhost:${port}`)
+  logger.info(`server listening at http://localhost:${port}`)
 );
 
 connectToDB();
-
-mySQLIsAlive();
--- back/src/common/db/mySqlDb.js
+++ back/src/common/db/mySqlDb.js
@@ -4,7 +4,10 @@ const {
   mySQL: { HOST, USER, DATABASE, PASSWORD },
 } = require('../config');
 
-const mySQLPool = mysql
+// -------------------------
+// create connection pool
+// -------------------------
+const dbPool = mysql
   .createPool({
     connectionLimit: 5,
     host: HOST,
@@ -14,16 +17,85 @@ const mySQLPool = mysql
   })
   .promise();
 
-const mySQLIsAlive = () => {
-  return mySQLPool
+// -------------------------
+// check db health
+// return promise with flag <dbIsAlive>
+// -------------------------
+const dbIsAlive = () => {
+  return dbPool
     .execute('select 1')
     .then(() => {
-      logger.info('mySQLisAlive:true');
-      return Promise.resolve('mySQLisAlive:true');
+      logger.info('dbIsAlive:success');
+      return Promise.resolve({ dbIsAlive: true });
     })
     .catch((err) => {
-      logger.error('mySQLisAlive:false ' + err);
+      logger.error('dbIsAlive:fail ' + err);
+      return Promise.resolve({ dbIsAlive: false });
     });
 };
 
-module.exports = { mySQLPool, mySQLIsAlive };
+// -------------------------
+// helper for reference population
+// -------------------------
+const loadOneColumnRefData = (data, tableRows, tableName) => {
+  data.forEach((name) => {
+    if (tableRows.filter((row) => row.name === name).length === 0) {
+      dbPool.execute(`insert into ${tableName} (name) values ("${name}")`);
+      logger.info(`expenseGroups table: inserted "${name}"`);
+    } else {
+      logger.info(`expenseGroups table: row exist "${name}"`);
+    }
+  });
+};
+
+// -------------------------
+// create tables
+// populate references
+// -------------------------
+dbIsAlive().then((res) => {
+  if (res.dbIsAlive) {
+    logger.info('check tables started');
+
+    const createExpenseGroup = `create table if not exists expenseGroups(
+      id int primary key auto_increment,
+      name varchar(255) not null
+    )`;
+    const expenseGroupData = ['Отпуск Калининград', 'Текущий бюджет', 'Покраска машины'];
+
+    const createExpenseItems = `create table if not exists expenseItems(
+      id int primary key auto_increment,
+      name varchar(255) not null
+    )`;
+    const expenseItemsData = [
+      'Самолет', 'Жилье', 'Экскурсии', 'Разное', 'Машина',
+      'Кафэ и рестораны', 'Продукты', 'Одежда', 'Обувь', 'Обучение',
+      'Бытовая химия', 'Косметика', 'Украшения', 'Расходники',
+      'Здоровье', 'Красота', 'Такси', 'Телефон', 'Интернет', 'Алкоголь',
+      'Танцы', 'Проезд', 'Проезд Эльзы', 'Сотовый', 'Очки', 'Товары для дома'
+    ];
+
+    dbPool
+      .execute(createExpenseGroup)
+      .then(() => {
+        logger.info('check tables: expenseGroup table exist');
+        return dbPool.execute(`select name from expenseGroups`);
+      })
+      .then(([rows]) => {
+        loadOneColumnRefData(expenseGroupData, rows, 'expenseGroups');
+      })
+      .then(() => {
+        dbPool.execute(createExpenseItems);
+      })
+      .then(() => {
+        logger.info('check tables: expenseItems table exist');
+        return dbPool.execute(`select name from expenseItems`);
+      })
+      .then(([rows]) => {
+        loadOneColumnRefData(expenseItemsData, rows, 'expenseItems');
+      });
+  } else {
+    logger.error(`Create and populate tables is fail. dbIsAlive:false`);
+  }
+});
+
+module.exports = {};
