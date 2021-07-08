fc4535ba604bfb650c54db0172ca86972c8f590e
michael-916310 <916310@rambler.ru>
2021-07-08 08:48:37 +0400

    feat: front: продолжаю делать список расходов

--- front/src/components/expenseList/expenseList.tsx
+++ front/src/components/expenseList/expenseList.tsx
@@ -1,42 +1,50 @@
 import React from 'react';
 
-import { Card, Descriptions } from 'antd';
+import { Button, Descriptions, Popconfirm } from 'antd';
+import { DeleteOutlined } from '@ant-design/icons';
 
 import { useAppSelector } from './../../app/hooks';
 
 import { IExpenseListItem } from './reducer';
 
+import styles from './expenseList.module.scss';
+
 export function ExpenseList(){
 
-  const list:[] = useAppSelector(state=>state.expenseList.expenseList);
+  const [deleteConfirmIndex, setDeleteConfirmIndex] = React.useState(-1);
+
+  const list:IExpenseListItem[] = useAppSelector(state=>state.expenseList.expenseList);
+
   return (
     <>
-      {/* { list.map((item:any)=>{
-        return (
-          <Card title={`${item.expenseName} ${item.amount}`}>
-            <p>месяц: {item.period}, день: {item.day}</p>
-            <p>{item.groupName}</p>
-            <p>{item.expenseName}</p>
-            <p>{item.description}</p>
-          </Card>
-        )
-      })} */}
         {list.map((item:IExpenseListItem) => {
             return (
-              <p>
-              <Descriptions
-                title={`Запись: ${item.id}`}
-                bordered
-                //column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
-                column={{ sm: 2, xs: 1 }}
-              >
-                <Descriptions.Item label="Статья">{item.expenseName}</Descriptions.Item>
-                <Descriptions.Item label="Сумма">{item.amount}</Descriptions.Item>
-                <Descriptions.Item label="Группа">{item.groupName}</Descriptions.Item>
-                <Descriptions.Item label="Дата">{item.period}-{item.day}</Descriptions.Item>
-                <Descriptions.Item label="Описание" span={2}>{item.description}</Descriptions.Item>
-
-              </Descriptions>
+              <p className={styles.itemContainer}>
+                <Descriptions
+                  bordered
+                  column={{ xl: 4, sm: 2, xs: 1 }}
+                  size="small"
+                  className={styles.description}
+                  extra = {
+                    <Popconfirm
+                      title="Title"
+                      visible ={item.id===deleteConfirmIndex}
+                    >
+                      <Button
+                        type="primary"
+                        onClick={()=>{setDeleteConfirmIndex(item.id)} }
+                      >
+                        <DeleteOutlined />
+                      </Button>
+                    </Popconfirm>
+                  }
+                >
+                  <Descriptions.Item label="Статья">{item.expenseName}</Descriptions.Item>
+                  <Descriptions.Item label="Сумма">{item.amount}</Descriptions.Item>
+                  <Descriptions.Item label="Группа">{item.groupName}</Descriptions.Item>
+                  <Descriptions.Item label="Дата">{item.period}-{item.day}</Descriptions.Item>
+                  <Descriptions.Item label="Описание" span={2}>{item.description}</Descriptions.Item>
+                </Descriptions>
               </p>
             )
         })}
