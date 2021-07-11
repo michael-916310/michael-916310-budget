064bf6c2f5903177097a5031e1c5dccd4dd80e0e
michael-916310 <916310@rambler.ru>
2021-07-11 15:21:53 +0400

    feat: front: по другому подключил redux dev tools & build

--- front/src/app/store.js
+++ front/src/app/store.js
@@ -1,6 +1,9 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import { reducer } from './reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
+
+import { reducer } from './reducers';
+
 
 import rootSaga from './sagas';
 
@@ -11,11 +14,18 @@ const sagaMiddleware = createSagaMiddleware();
 //   applyMiddleware(sagaMiddleware)
 // );
 
+// export const store = createStore(
+//   reducer,
+//   compose(
+//     applyMiddleware(sagaMiddleware),
+//     window?.__REDUX_DEVTOOLS_EXTENSION__ && window?.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// );
+
 export const store = createStore(
   reducer,
-  compose(
+  composeWithDevTools(
     applyMiddleware(sagaMiddleware),
-    window?.__REDUX_DEVTOOLS_EXTENSION__ && window?.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
